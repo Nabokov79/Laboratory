c00fce2242620c0157e9d1008841ceb32c4c1441
Nabokov79 <nabokovsg@yandex.ru>
2024-02-09 19:30:03 +0300

    add

---
+++ file/src/main/java/ru/nabokovsg/file/FileApplication.java
@@ -0,0 +1,14 @@
+package ru.nabokovsg.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+
+@SpringBootApplication
+public class FileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileApplication.class, args);
+    }
+}
---
+++ lab_nk/src/main/java/ru/nabokovsg/lab_nk/LabNKApplication.java
@@ -0,0 +1,12 @@
+package ru.nabokovsg.lab_nk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabNKApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabNKApplication.class, args);
+    }
+}
