320b024ebe47766195314c4b37b00b70581f9154
NabokovSG <nabokovsg@yandex.ru>
2024-02-29 16:42:53 +0300

    add fields

--- gateway/src/main/java/ru/nabokovsg/gateway/dto/labNk_service/headerDocument/NewHeaderDocumentDto.java
+++ gateway/src/main/java/ru/nabokovsg/gateway/dto/labNk_service/headerDocument/NewHeaderDocumentDto.java
@@ -1,4 +1,26 @@
 package ru.nabokovsg.gateway.dto.labNk_service.headerDocument;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Schema(description = "Данные для добавления вида документа")
 public class NewHeaderDocumentDto {
+
+    @Schema(description = "Название документа")
+    @NotBlank(message = "title should not be blank")
+    private String title;
+    @Schema(description = "Заголовок документа")
+    @NotBlank(message = "heading should not be blank")
+    private String heading;
+    @Schema(description = "Тип документа")
+    @NotBlank(message = "document type should not be blank")
+    private String typeDocument;
 }
--- gateway/src/main/java/ru/nabokovsg/gateway/dto/labNk_service/headerDocument/UpdateHeaderDocumentDto.java
+++ gateway/src/main/java/ru/nabokovsg/gateway/dto/labNk_service/headerDocument/UpdateHeaderDocumentDto.java
@@ -1,4 +1,30 @@
 package ru.nabokovsg.gateway.dto.labNk_service.headerDocument;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@Schema(description = "Данные для изменения вида документа")
 public class UpdateHeaderDocumentDto {
+
+    @Schema(description = "Индентификатор")
+    @NotNull(message = "id should not be null")
+    @Positive(message = "id can only be positive")
+    private Long id;
+    @Schema(description = "Название документа")
+    @NotBlank(message = "title should not be blank")
+    private String title;
+    @Schema(description = "Заголовок документа")
+    @NotBlank(message = "heading should not be blank")
+    private String heading;
+    @Schema(description = "Тип документа")
+    @NotBlank(message = "document type should not be blank")
+    private String typeDocument;
 }
