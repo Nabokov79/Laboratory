2308df5a7bfe7ebcea8eb3f6736f20b6191cf27c
NabokovSG <nabokovsg@yandex.ru>
2024-02-29 16:41:29 +0300

    add mappers

--- lab_nk/src/main/java/ru/nabokovsg/lab_nk/mappers/TaskJournalMapper.java
+++ lab_nk/src/main/java/ru/nabokovsg/lab_nk/mappers/TaskJournalMapper.java
@@ -1,7 +1,15 @@
 package ru.nabokovsg.lab_nk.mappers;
 
 import org.mapstruct.Mapper;
+import ru.nabokovsg.lab_nk.dto.taskJournal.FullTaskJournalDto;
+import ru.nabokovsg.lab_nk.dto.taskJournal.TaskJournalDto;
+import ru.nabokovsg.lab_nk.models.TasksJournal;
 
 @Mapper(componentModel = "spring")
 public interface TaskJournalMapper {
+
+    TasksJournal mapToTaskJournal(TaskJournalDto taskJournalDto, String branch
+                                , String building, String address, String equipment);
+
+    FullTaskJournalDto mapToFullTasksJournalDto(TasksJournal tasksJournal);
 }
