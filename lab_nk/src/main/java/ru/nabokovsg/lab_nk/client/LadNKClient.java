6b256c95d7d57fc904c921ddc9352350db819de6
NabokovSG <nabokovsg@yandex.ru>
2024-02-29 16:43:16 +0300

    add

--- lab_nk/src/main/java/ru/nabokovsg/lab_nk/client/LadNKClient.java
+++ lab_nk/src/main/java/ru/nabokovsg/lab_nk/client/LadNKClient.java
@@ -1,56 +1,34 @@
 package ru.nabokovsg.lab_nk.client;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
+import ru.nabokovsg.lab_nk.client.dto.FullBranchDto;
+import ru.nabokovsg.lab_nk.client.dto.FullEquipmentDto;
 import ru.nabokovsg.lab_nk.client.dto.ShortEmployeeDto;
-import ru.nabokovsg.lab_nk.dto.employees.DivisionDataDto;
-import ru.nabokovsg.lab_nk.models.enums.DivisionType;
 
 import java.util.List;
 
 @Service
-public class LadNKClient extends CompanyClient{
+@RequiredArgsConstructor
+public class LadNKClient {
 
+    private final CompanyClient companyClient;
+    private final EquipmentClient equipmentClient;
     private static final String DELIMITER = "/";
     private static final String API_PREFIX_EMPLOYEE = "/employee";
+    private static final String API_PREFIX_BUILDING = "/building";
+    private static final String API_PREFIX_EQUIPMENT = "/equipments";
 
-    public LadNKClient(@Value("${company-server.url}") String baseUrl) {
-        super(WebClient.builder()
-                .baseUrl(baseUrl)
-                .build());
+    public List<ShortEmployeeDto> getAllEmployee(Long id, String divisionType) {
+        return companyClient.getAllEmployees(String.join(DELIMITER, API_PREFIX_EMPLOYEE,
+                "all", String.valueOf(id)), "divisionType", divisionType);
     }
 
-    public List<ShortEmployeeDto> getAllEmployee(DivisionDataDto divisionDataDto) {
-        return getAll(String.join(DELIMITER,  API_PREFIX_EMPLOYEE, "all",  getDivisionId(divisionDataDto))
-                , "divisionType", getDivisionType(divisionDataDto));
+    public FullBranchDto getBranch(Long id) {
+        return companyClient.getBranch(String.join(DELIMITER, API_PREFIX_BUILDING, String.valueOf(id)));
     }
 
-    private String getDivisionId(DivisionDataDto divisionDataDto) {
-        Long id = null;
-        if (divisionDataDto.getOrganizationId() != null) {
-            id = divisionDataDto.getOrganizationId();
-        }
-        if (divisionDataDto.getBranchId() != null) {
-            id = divisionDataDto.getBranchId();
-        }
-        if (divisionDataDto.getDepartmentId() != null) {
-            id = divisionDataDto.getDepartmentId();
-        }
-        return String.valueOf(id);
-    }
-
-    private String getDivisionType(DivisionDataDto divisionDataDto) {
-        DivisionType divisionType = null;
-        if (divisionDataDto.getOrganizationId() != null) {
-            divisionType = DivisionType.ORGANIZATION;
-        }
-        if (divisionDataDto.getBranchId() != null) {
-            divisionType = DivisionType.BRANCH;
-        }
-        if (divisionDataDto.getDepartmentId() != null) {
-            divisionType = DivisionType.DEPARTMENT;
-        }
-        return String.valueOf(divisionType);
+    public FullEquipmentDto getEquipment(Long id) {
+        return equipmentClient.getEquipment(String.join(DELIMITER, API_PREFIX_EQUIPMENT, String.valueOf(id)));
     }
 }
