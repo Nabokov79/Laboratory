e17f2e8826218306e70ef1bcd752c42b10cea469
NabokovSG <nabokovsg@yandex.ru>
2024-02-29 16:41:43 +0300

    add method

--- lab_nk/src/main/java/ru/nabokovsg/lab_nk/repository/TaskJournalRepository.java
+++ lab_nk/src/main/java/ru/nabokovsg/lab_nk/repository/TaskJournalRepository.java
@@ -3,5 +3,9 @@ package ru.nabokovsg.lab_nk.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.nabokovsg.lab_nk.models.TasksJournal;
 
+import java.time.LocalDate;
+
 public interface TaskJournalRepository extends JpaRepository<TasksJournal, Long> {
+
+    TasksJournal findByDateAndEquipmentId(LocalDate date, Long equipmentId);
 }
